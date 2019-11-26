5dae02d6b6655cf41413f32f7fca02ca2eb58d08
Tondon César <pilou.tondon@hotmail.fr>
2019-11-26 15:11:46 +0100

    Ajout du index.php avec ses routes associées

--- wishlist/index.php
+++ wishlist/index.php
@@ -1,1 +1,17 @@
 <?php
+
+$app = new \Slim\Slim();
+
+$app->get('/liste/afficher', function (){
+    echo "get : afficherListe";
+});
+
+$app->get('/liste/afficher/itemdeliste', function() {
+    echo "get : afficherItemDeListe";
+});
+
+$app->get('/item/id', function() {
+    echo "get : designerItemParID";
+});
+
+$app->run();
