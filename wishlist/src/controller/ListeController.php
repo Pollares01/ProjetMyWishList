0addf290f08a13922182a91f39687cae76e5a02c
Tondon César <pilou.tondon@hotmail.fr>
2019-12-02 11:57:23 +0100

    dernier td fini avec test sur le index href

--- wishlist/src/controller/ListeController.php
+++ wishlist/src/controller/ListeController.php
@@ -4,20 +4,18 @@ use wishlist\modele\Liste;
 use wishlist\vue\VueParticipant;
 class ListeController
 {
-    public static function afficherListe()
-    {
+    public static function afficherListe() {
         $list = Liste::get();
-        $vue = new VueParticipant($list);
+        $vue = new VueParticipant($list, 'LIST_LISTE_SOUHAITS');
+        $vue->render();
         }
-    }
 
-    public static function afficherItemDeListe($no)
-    {
+    public static function afficherItemDeListe($no) {
         $liste = Liste::where('no', '=', $no)->first();
         $items2 = $liste->items()->get();
-        foreach ($items2 as $value) {
-            echo($value);
-            echo "<br>";
-        }
+        $vue1 = new VueParticipant($liste, 'LIST_LISTE_SOUHAITS');
+        $vue2 = new VueParticipant($items2, 'LIST_LISTE_SOUHAITS_ITEMS');
+        $vue1->render();
+        $vue2->render();
     }
 }
