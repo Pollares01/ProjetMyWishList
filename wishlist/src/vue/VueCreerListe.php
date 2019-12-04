cc915074075b8907d32bc2265a322578a71bfe50
Tondon César <pilou.tondon@hotmail.fr>
2019-12-04 15:57:44 +0100

    nav bar vue creer liste

--- wishlist/src/vue/VueCreerListe.php
+++ wishlist/src/vue/VueCreerListe.php
@@ -4,7 +4,7 @@ namespace wishlist\vue;
 
 class VueCreerListe {
     
-    private $urlAfficherToutesListes, $urlAfficherItemsListe, $urlTousItem, $urlITemID, $urlCreerListe;
+    private $urlAfficherToutesListes, $urlAfficherItemsListe, $urlTousItem, $urlITemID, $urlCreerListe, $urlPageIndex;
 
     public function __construct() {
 
@@ -24,6 +24,8 @@ class VueCreerListe {
         $itemUrl4 = $this->app->urlFor('creer_liste');
         $this->urlCreerListe = $itemUrl4;
 
+        $this->urlPageIndex = $this->app->urlFor('page_index');
+
         $this->URLimages = $this->app->request->getRootUri() . '/img/';
         $this->URLbootstrapCSS = $this->app->request->getRootUri() . '/public/bootstrap.css';
         $this->URLbootstrapJS = $this->app->request->getRootUri() . '/public/boostrap.min.js';
@@ -38,64 +40,71 @@ class VueCreerListe {
             </head>
             <body>
                 <header>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">WishList</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                    <li class="nav-item">
-                      <a class="nav-link" href="$this->urlAfficherToutesListes">Affichage des listes</a>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+                  <div class="container">
+                    <a class="navbar-brand" href="$this->urlPageIndex">My Wish List</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                      <ul class="navbar-nav ml-auto">
+                      <li class="nav-item">
+                          <a class="nav-link" href="$this->urlPageIndex">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="$this->urlAfficherToutesListes">Afficher la liste des listes
+                              </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="$this->urlTousItem">Afficher la liste des items</a>
+                        </li>
+                       <li class="nav-item">
+                      <a class="nav-link" href="$this->urlITemID">Affichage d'un item par id</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="$this->urlAfficherItemsListe">Affichage des items d'une liste</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="$this->urlTousItem">Affichade de la liste de tous les items</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="$this->urlCreerListe">Creer une liste de souhait</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
+                    <a class="nav-link" href="$this->urlCreerListe">Creer une liste de souhait</a>
+                  </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
                 </header>
-                <div>
-                <form>
-                <div class="form-row">
-                    <div class="col-md-4 mb-3">
-                    <label for="validationServer01">Titre</label>
-                    <input type="text" class="form-control is-valid" id="validationServer01" value="Exemple : Pour fêter le bac !" required>
-                    <div class="valid-feedback">
-                        Cela semble bon!
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                           <div class="col-12 text-center">
+                           <form>
+                           <div class="form-row">
+                               <div class="col-md-4 mb-3">
+                               <label for="validationServer01">Titre</label>
+                               <input type="text" class="form-control is-valid" id="validationServer01" value="Exemple : Pour fêter le bac !" required>
+                               <div class="valid-feedback">
+                                   Cela semble bon!
+                               </div>
+                               </div>
+                           </div>
+                           <div class="form-group">
+                               <label for="exampleFormControlTextarea1">Description</label>
+                               <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" value="ex" required></textarea>
+                           </div>
+                           <div class="form-group">
+                           <label for="exampleFormControlFile1">Choisir une image depuis votre ordinateur</label>
+                           <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                           </div>
+                           <div class="form-group">
+                               <div class="form-check">
+                               <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
+                               <label class="form-check-label" for="invalidCheck3">
+                                   Accepter les termes et conditions
+                               </label>
+                               <div class="invalid-feedback">
+                               Vous devez accepter avant de soumettre.
+                               </div>
+                               </div>
+                           </div>
+                           <button class="btn btn-primary" type="submit">Créer</button>
+                           </form>
+                           </div>
                     </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Description</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" value="ex" required></textarea>
-                </div>
-                <div class="form-group">
-                <label for="exampleFormControlFile1">Choisir une image depuis votre ordinateur</label>
-                <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                </div>
-                <div class="form-group">
-                    <div class="form-check">
-                    <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-                    <label class="form-check-label" for="invalidCheck3">
-                        Accepter les termes et conditions
-                    </label>
-                    <div class="invalid-feedback">
-                    Vous devez accepter avant de soumettre.
-                    </div>
-                    </div>
-                </div>
-                <button class="btn btn-primary" type="submit">Créer</button>
-                </form>
                 </div>
                 <script src="$this->URLbootstrapJS"></script>
             </body>
