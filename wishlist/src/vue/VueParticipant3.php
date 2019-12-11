7ae5f3f4de453402997ef8ffc285f69a828eb1ad
Paul KELBERT <paul.kelbert3@etu.univ-lorraine.fr>
2019-12-11 15:10:47 +0100

    Reprise affichage de toutes les listes

--- wishlist/src/vue/VueParticipant3.php
+++ wishlist/src/vue/VueParticipant3.php
@@ -65,18 +65,19 @@ class VueParticipant3
      * @return string
      */
     private function affichageToutListe() {
-        $res = '
-            <section>';
+        $res = '';
         foreach ($this->liste as $value){
-            $res = $res . "<tr>
-                              <th scope=\"row\">$value->no</th>
-                              <td>$value->user_id</td>
-                              <td>$value->titre</td>
-                              <td>$value->description</td>
-                           </tr>";
+            $lien = $this->app->urlFor('afficher_items_dune_liste', ['no' => $value->no]);
+            $res = $res . "
+                                <a href=\"$lien\" class='text-black-50'>
+                                    <div class='bg-light shadow'>
+                                    $value->titre  
+                                    </div>
+                                </a><br><br>
+                           ";
         }
-        $res = $res . '</section> ';
-        return "<h1> liste de tout : </h1> $res";
+        $res = $res . '';
+        return "<h1> Liste des Listes : </h1> $res";
     }
 
     /**
@@ -107,18 +108,8 @@ class VueParticipant3
         switch ($this->typeAff){
             //cas où l'on veut afficher toutes les listes
             case 'ALL_LISTE' : {
-                $content = "<table class=\"table\">
-                                <thead class=\"thead-dark\">
-                                <tr>
-                                    <th scope=\"col\">Numéro</th>
-                                    <th scope=\"col\">User id</th>
-                                    <th scope=\"col\">Titre</th>
-                                    <th scope=\"col\">Description</th>
-                                </tr>
-                                </thead>
-                                <tbody>";
-                $content = $content . $this->affichageToutListe();
-                $content = $content . "</tbody></table>";
+
+                $content = $this->affichageToutListe();
                 break;
             }
             //cas où l'on veut afficher les items d'une liste
@@ -141,6 +132,9 @@ class VueParticipant3
         <html>
             <head>
                 <link rel="stylesheet" href="$this->URLbootstrapCSS">
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
             </head>
             <body>
                 <header>
